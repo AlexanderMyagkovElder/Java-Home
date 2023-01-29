de4d875acb545f83fd88a7e0901dc966a0e08135
Alexander Myagkov <anistonbale@gmail.com>
2023-01-30 00:21:19 +0300

    add2

---
+++ src/Sem2/Tsk1.java
@@ -0,0 +1,26 @@
+package Sem2;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Tsk1 {
+    public static void main(String[] args) {
+//        File log = new File("log.txt");
+        int[] arr = new int[]{3, 9, 4, 5, 2, 7, 1, 6};
+        boolean isWorked=false;
+        while (!isWorked) {
+            isWorked=true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    isWorked=false;
+                    int temp2 = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp2;
+                    }
+
+
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }}
